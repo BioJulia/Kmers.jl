e4eecdd0b4aae9153975737ec862b91c666b397c
Ben J. Ward <ward9250@gmail.com>
2017-06-02 04:28:50 +0100

    added kmer types and aliases. (#2)
    (part 1 of many)

--- src/Kmers.jl
+++ src/Kmers.jl
@@ -1,5 +1,17 @@
+# Kmers.jl
+# ========
+#
+# Module for kmers and short sequence fragments.
+#
+# This file is a part of the Kmers.jl, a package in the BioJulia ecosystem.
+# License is MIT: https://github.com/BioJulia/Bio.jl/blob/master/LICENSE.md
+
+__precompile__()
+
 module Kmers
 
-# package code goes here
+import BioSymbols: NucleicAcid, DNA, RNA
+
+include("kmer.jl")
 
 end # module
